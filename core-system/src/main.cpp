ebeb3becaeb5a3876d3c4e0341c44902d9c3d081
avainfo <avainfoconseils@gmail.com>
2026-05-08 01:57:03 +0100

    feat(struct): add the basic structures for screen configuration

--- core-system/src/main.cpp
+++ core-system/src/main.cpp
@@ -17,10 +17,44 @@
  */
 
 #include <iostream>
+#include <filesystem>
+#include <string>
+#include <yaml-cpp/node/node.h>
+#include <yaml-cpp/node/parse.h>
+#include <yaml-cpp/yaml.h>
 
 using namespace std;
 
+namespace fs = filesystem;
+
+struct Canva {
+	int width, height;
+};
+
+struct Screen {
+	string id;
+	int x, y;
+	int width, height;
+};
+
+struct ScreensConfig {
+	Canva canvas;
+	vector<Screen> screens;
+};
+
 int main ([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+	fs::path config_dir = "config/local";
+
+	for (int i = 1; i < argc; ++i) {
+		const string arg = argv[i];
+
+		if (arg == "--config-dir" && i + 1 < argc) {
+			config_dir = argv[++i];
+		}
+	}
+
 	cout << "Hello World" << endl;
+	YAML::Node screens_config = YAML::LoadFile((config_dir / "screens.yaml").string());
+	cout << screens_config["canvas"] << endl;
 	return 0;
 }
