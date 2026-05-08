c44806f86faf55597e0a120e75b4dbd8f4c9c4ea
avainfo <avainfoconseils@gmail.com>
2026-05-08 01:03:57 +0100

    feat(main): Add default cpp code.

---
+++ core-system/src/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Ava Info Conseils
+ *
+ * Author: Antonin Do Souto
+ * Contact: antonindosouto@gmail.com
+ *
+ * Proprietary / Commercial License
+ *
+ * Copyright (c) 2026 Antonin Do Souto
+ *
+ * All rights reserved.
+ *
+ * This repository contains proprietary software and documentation.
+ * Unless explicitly stated otherwise in a subdirectory LICENSE file,
+ * all files in this repository are proprietary and may not be copied,
+ * modified, distributed, or used without prior written permission.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main ([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+	cout << "Hello World" << endl;
+	return 0;
+}
