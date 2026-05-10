1d334d10f55a3fde8e1c71e9d232280ac695d98e
avainfo <avainfoconseils@gmail.com>
2026-05-10 01:24:19 +0100

    chore(license): add proprietary license header to screen configuration
    Prepend the standard Ava Info Conseils proprietary and commercial license block to `screen_configuration.hpp` to explicitly define copyright ownership, authorship, and usage restrictions.

--- core-system/include/screen_configuration.hpp
+++ core-system/include/screen_configuration.hpp
@@ -1,3 +1,21 @@
+/*
+ * Ava Info Conseils
+ *
+ * Author: Antonin Do Souto
+ * Contact: antonindosouto@gmail.com
+ *
+ * Proprietary / Commercial License
+ *
+ * Copyright (c) 2026 Antonin Do Souto
+ *
+ * All rights reserved.
+ *
+ * This repository contains proprietary software and documentation.
+ * Unless explicitly stated otherwise in a subdirectory LICENSE file,
+ * all files in this repository are proprietary and may not be copied,
+ * modified, distributed, or used without prior written permission.
+ */
+
 #pragma once
 #include "yaml-cpp/node/node.h"
 #include <string>
